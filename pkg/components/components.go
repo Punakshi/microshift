9445173262cf39291b0f7ed6449fd657700ee84d
Punakshi <punakshi.chaand.pc@gmail.com>
2022-06-10 09:15:15 -0700

    Basic flannel removal test

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -24,9 +24,9 @@ func StartComponents(cfg *config.MicroshiftConfig) error {
 		klog.Warningf("Failed to start DNS controller: %v", err)
 		return err
 	}
-	if err := startFlannel(cfg.DataDir + "/resources/kubeadmin/kubeconfig"); err != nil {
+	/*if err := startFlannel(cfg.DataDir + "/resources/kubeadmin/kubeconfig"); err != nil {
 		klog.Warningf("Failed to start Flannel: %v", err)
 		return err
-	}
+	}*/
 	return nil
 }
--- pkg/release/release.go
+++ pkg/release/release.go
@@ -19,11 +19,11 @@ package release
 var Base = "4.10.16"
 
 var Image = map[string]string{
-	"cli":                           "quay.io/microshift/cli:" + Base,
-	"coredns":                       "quay.io/microshift/coredns:" + Base,
-	"haproxy_router":                "quay.io/microshift/haproxy-router:" + Base,
-	"kube_flannel":                  "quay.io/microshift/flannel:" + Base,
-	"kube_flannel_cni":              "quay.io/microshift/flannel-cni:v0.14.0",
+	"cli":            "quay.io/microshift/cli:" + Base,
+	"coredns":        "quay.io/microshift/coredns:" + Base,
+	"haproxy_router": "quay.io/microshift/haproxy-router:" + Base,
+	//"kube_flannel":                  "quay.io/microshift/flannel:" + Base,
+	//"kube_flannel_cni":              "quay.io/microshift/flannel-cni:v0.14.0",
 	"kube_rbac_proxy":               "quay.io/microshift/kube-rbac-proxy:" + Base,
 	"kubevirt_hostpath_provisioner": "quay.io/microshift/hostpath-provisioner:" + Base,
 	"pause":                         "quay.io/microshift/pause:" + Base,
